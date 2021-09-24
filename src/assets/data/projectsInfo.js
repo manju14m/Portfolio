5c6141eeaa9909ab8f7f7968506f800fc34523c7
manju14m <manju14m@hotmail.com>
2021-09-24 13:52:12 +0530

    added project images and links

--- src/assets/data/projectsInfo.js
+++ src/assets/data/projectsInfo.js
@@ -1,36 +1,37 @@
 import random from "../media/random.png";
-import todo from "../media/todo.png";
+import taskManager from "../media/TaskManager.png";
+import shopify from "../media/shopify.png";
 
 const projectData = [
   {
     name: "E-Commerce App",
     info: "This app is developed using ReactJS with firebase authentication.",
-    TechStack:
-      "ReactJS, JavaScript, SCSS, Firebase authentication, Firestore DB.",
-    url: "https://manju-react-todolist.netlify.app",
-    image: random,
+    TechStack: "ReactJS, JavaScript, SCSS, Firebase authentication.",
+    url: "https://manju-shopify.netlify.app/",
+    image: shopify,
   },
+  // {
+  //   name: "Messaging App",
+  //   info: "This is a realtime chatting application with firebase authentication and fireStore database.",
+  //   TechStack:
+  //     "ReactJS, JavaScript, Material UI, SCSS, Firebase authentication, FireStore DB.",
+  //   url: "https://manju-react-todolist.netlify.app",
+  //   image: todo,
+  // },
   {
-    name: "Messaging App",
-    info: "This is a realtime chatting application with firebase authentication and firestore database.",
-    TechStack:
-      "ReactJS, JavaScript, Material UI, SCSS, Firebase authentication, Firestore DB.",
-    url: "https://manju-react-todolist.netlify.app",
-    image: todo,
+    name: "Task Manager",
+    info: "MERN Stack based Task MAnager application",
+    TechStack: "ReactJS, NodeJS, ExpressJS, MongoDB, JavaScript, SCSS.",
+    url: "https://taskmanager-gold.vercel.app",
+    image: taskManager,
   },
+
   {
     name: "Random Quote Generator",
-    info: "This is a RaectJS based random quote generator.",
+    info: "This is a ReactJS based random quote generator.",
     TechStack: "ReactJS, JavaScript, SCSS.",
     url: "https://manju-react-todolist.netlify.app",
     image: random,
-  },
-  {
-    name: "ToDo App",
-    info: "This is a ReactJS based ToDo application",
-    TechStack: "ReactJS, JavaScript, SCSS.",
-    url: "https://manju-react-todolist.netlify.app",
-    image: todo,
   },
 ];
 
